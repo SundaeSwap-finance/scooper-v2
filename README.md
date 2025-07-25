6bc7890517352f1dc65522f116eee4dc9665a94b
ruko <rukokarasu@gmail.com>
2025-07-25 16:33:16 -0700

    chainsync intersect origin

---
+++ README.md
@@ -0,0 +1,1 @@
+cargo run -- --addr IP:PORT --name buko --magic 2
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
 use clap::Parser;
+use pallas_network::facades::PeerClient;
+use pallas_network::miniprotocols::{PROTOCOL_N2N_CHAIN_SYNC};
+use pallas_network::multiplexer;
+use pallas_network::miniprotocols::chainsync;
+use tokio::sync::mpsc;
 
 #[derive(Parser, Debug)]
 struct Args {
@@ -7,12 +12,27 @@ struct Args {
 
     #[arg(short, long, default_value_t = 1)]
     count: u8,
+
+    #[arg(short, long)]
+    addr: String,
+
+    #[arg(short, long)]
+    magic: u64,
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let args = Args::parse();
 
     for _ in 0..args.count {
         println!("Hello {}!", args.name);
     }
+
+    let handle = tokio::spawn(async move {
+        let mut peer_client = PeerClient::connect(args.addr, args.magic).await.unwrap();
+        let chainsync = peer_client.chainsync();
+        let intersect_result = chainsync.intersect_origin().await;
+        println!("Intersect result {:?}", intersect_result);
+    });
+    handle.await;
 }
