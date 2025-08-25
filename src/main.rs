d115c11100ef4ef46902b460a2a071be239a8dad
ruko <rukokarasu@gmail.com>
2025-08-25 16:00:38 -0700

    decode transaction

--- src/main.rs
+++ src/main.rs
@@ -32,6 +32,28 @@ fn decode_header_point(header_content: &HeaderContent) -> Result<Point, String> 
     return Err("cannot decode header point".to_owned());
 }
 
+type BlockWrapper = (u16, alonzo::Block);
+
+fn decode_alonzo_block(content: &[u8]) -> Option<alonzo::Block> {
+    let as_alonzo_block: Result<BlockWrapper, minicbor::decode::Error> =
+        minicbor::decode(content);
+    match as_alonzo_block {
+        Ok(b) => Some(b.1),
+        Err(_) => None,
+    }
+}
+
+fn handle_alonzo_block(block: alonzo::Block) {
+    for body in block.transaction_bodies {
+        for input in body.inputs {
+            println!("Spent {:?}", input);
+        }
+        for output in body.outputs {
+            println!("Produced {:?}", output);
+        }
+    }
+}
+
 #[tokio::main]
 async fn main() {
     let args = Args::parse();
@@ -46,7 +68,11 @@ async fn main() {
                 NextResponse::RollForward(content, tip) => {
                     let point = decode_header_point(&content).unwrap();
                     let resp = peer_client.blockfetch().fetch_single(point).await.unwrap();
-                    println!("RollForward({}, {:?})", hex::encode(resp), tip);
+                    if let Some(alonzo_block) = decode_alonzo_block(&resp) {
+                        handle_alonzo_block(alonzo_block);
+                    } else {
+                        println!("RollForward({}, {:?})", hex::encode(resp), tip);
+                    }
                 },
                 NextResponse::RollBackward(point, tip) => {
                     println!("RollBackward({:?}, {:?})", point, tip);
